8e58fe03958df1954d0a49483605f8bfdcc9d522
David Rachwalik <david.rachwalik@outlook.com>
2022-01-16 14:38:28 -0600

    Exercise 2.3 - Routes

---
+++ week-2/routes/rachwalik-routes.js
@@ -0,0 +1,29 @@
+/*
+    Title: rachwalik-routes.js
+    Author: David Rachwalik
+    Date: 2022/01/16
+    Description: Node.js server for WEB-330 site
+*/
+
+var express = require("express");
+var http = require("http");
+var app = express();
+
+app.get("/", function (request, response) {
+    response.end("Welcome to the homepage!");
+});
+app.get("/about", function (request, response) {
+    response.end("Welcome to the about page!");
+});
+app.get("/contact", function (request, response) {
+    response.end("Welcome to the contact page!");
+});
+app.use(function (request, response) {
+    response.statusCode = 404;
+    response.end("404!");
+});
+http.createServer(app).listen(8080);
+
+// --- Demo Steps ---
+// * run server with command: node rachwalik-routes.js
+// * navigate to localhost:8080 or enter the command: curl -i localhost:8080
