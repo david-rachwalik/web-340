055d98482ef53931be951cd2d25b1640bc560ef9
David Rachwalik <david.rachwalik@outlook.com>
2022-01-16 14:35:34 -0600

    minor spacing touchup

--- week-2/hello-world/rachwalik-hello-world.js
+++ week-2/hello-world/rachwalik-hello-world.js
@@ -10,10 +10,8 @@ let express = require("express");
 let http = require("http");
 
 var app = express();
-
 app.use(function (request, response) {
     console.log("In comes a request to: " + request.url);
-
     response.end("Hello World");
 });
 
