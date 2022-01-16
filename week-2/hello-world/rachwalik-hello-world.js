382dd9eb4bcaa2ce845fd6b57b26f53a36a979c6
David Rachwalik <david.rachwalik@outlook.com>
2022-01-16 14:24:49 -0600

    Exercise 2.2 - Hello World with Express

---
+++ week-2/hello-world/rachwalik-hello-world.js
@@ -0,0 +1,24 @@
+/*
+    Title: rachwalik-hello-world.js
+    Author: David Rachwalik
+    Date: 2022/01/16
+    Description: Node.js server for WEB-330 site
+ */
+
+// Import declaration
+let express = require("express");
+let http = require("http");
+
+var app = express();
+
+app.use(function (request, response) {
+    console.log("In comes a request to: " + request.url);
+
+    response.end("Hello World");
+});
+
+http.createServer(app).listen(8080);
+
+// --- Demo Steps ---
+// * run server with command: node rachwalik-hello-world.js
+// * navigate to localhost:8080 or enter the command: curl -i localhost:8080
