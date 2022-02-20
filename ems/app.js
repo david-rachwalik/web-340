3112446c484c4e38ad851dd6886fb85c12815a03
David Rachwalik <david.rachwalik@outlook.com>
2022-02-20 11:37:17 -0600

    database actions added

--- ems/app.js
+++ ems/app.js
@@ -8,16 +8,53 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const mongoose = require('mongoose');
 const logger = require('morgan');
+const Fruit = require('./models/fruit.js');
+const Employee = require('./models/employee.js');
 
-// Setup server views and logger
+// --- Database Setup Steps ---
+
+// Build database connection string
+// const username = 'admin';
+const database = 'ems';
+// const mongoDB = `mongodb+srv://${username}:${password}@buwebdev-cluster-1.gfevl.mongodb.net/${database}?retryWrites=true&w=majority`;
+const mongoDB = `mongodb+srv://buwebdev-cluster-1.gfevl.mongodb.net/${database}`;
+
+// Setup connection to MongoDB
+mongoose.connect(mongoDB, {
+  useMongoClient: true,
+});
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+
+// Database error handling
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+
+// Actions to perform once database connection is ready
+db.once('open', () => {
+  console.log('Application connected to mLab MongoDB instance');
+});
+
+// --- Application Setup Steps ---
+
+// Setup app server, view engine, logger, static files
 const app = express();
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('short'));
 app.use(express.static(`${__dirname}/public`)); // declare static directory
 
-// Setup server routes
+// Instantiate models
+const fruit = new Fruit({
+  name: 'Apple',
+});
+const employee = new Employee({
+  firstName: 'David',
+  lastName: 'Rachwalik',
+});
+
+// Setup server routes (views)
 app.get('/', (request, response) => {
   response.render('index', {
     title: 'Home page',
